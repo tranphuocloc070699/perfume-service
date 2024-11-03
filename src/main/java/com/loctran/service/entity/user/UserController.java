77008f55b39c5d36383581d56afc99d75b640947
tranphuocloc <tranphuocloc070699@gmail.com>
2024-11-03 23:20:53 +0700

    feat:home

--- src/main/java/com/loctran/service/entity/user/UserController.java
+++ src/main/java/com/loctran/service/entity/user/UserController.java
@@ -133,20 +133,22 @@ public class UserController {
   }
 
   @PostMapping("/upload")
-  public ResponseEntity<ResponseDto> upload(@RequestParam("image") MultipartFile multipartFile,
-      @RequestParam("id") String id) {
+  public ResponseEntity<ResponseDto> upload(@RequestParam("image") MultipartFile multipartFile) {
     String filename = multipartFile.getOriginalFilename();
-    String uploadDir = "src/main/resources/static/upload/" + id;
+    String uploadDir = "";
     try {
       String path = FileUploadUtil.saveFile(uploadDir, filename, multipartFile);
 
-
+      System.out.println("path: "+ path);
 
       ResponseDto responseDto = ResponseDto.builder()
           .message("Upload file thành công").status(200).data(path).build();
 
       return ResponseEntity.ok(responseDto);
     } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+    catch (RuntimeException e) {
       throw new RuntimeException(e);
     }
   }
